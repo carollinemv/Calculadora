0d3dda44213cd8ba866cc43da910993c39682a15
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-10-26 15:40:07 -0300

    Add files via upload

---
+++ Calculadora de 4 operaçoes/calculadora.js
@@ -0,0 +1,16 @@
+const x = prompt("Informe o primeiro número:")
+const y = prompt("Informe o segundo número:")
+
+const soma = x + y
+const subtracao = x - y
+const multiplicacao = x * y
+const divisao = x / y
+
+alert(
+    "Resultados:\n" +
+    "\nSome: " + soma +
+    "\nSubtração: " + subtracao +
+    "\nMultiplicação " + multiplicacao +
+    "\nDivisão: " + divisao
+
+)
